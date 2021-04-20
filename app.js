8dffa5c5dfa58b80210e4a5ef1e88fcdf73559e6
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2021-04-20 21:26:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const Password = require('./models/passwordList');
 const User = require('./models/userList');
 const bodyParser = require('body-parser');
 const passwordRoutes = require('./routes/passwordRoutes');
+const sendEmail = require('./email/email');
 //var cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoDBsession = require("connect-mongoDB-session")(session);
 const bcrypt =  require('bcrypt');
+const crypto = require('crypto');
 var morgan = require("morgan");
-const path= require('path');
+//const path= require('path');
+
+
 //express app
 const app = express();
 const router = express.Router();
@@ -114,12 +118,13 @@ app.post('/signup', async (req, res) => {
 // route for user Login
 app.get("/login", (req, res) => { 
     res.render('login', { title:"login Page"});
+    
 });
 
 // route for user Login
 app.post("/login", async (req, res) => {
     const {email, password} = req.body;
- 
+
     // Check required fields
     if(!email || !password){
         console.log('Please fill in all fields');
@@ -127,7 +132,7 @@ app.post("/login", async (req, res) => {
     }
 
         // Passed Validation
-    User.findOne({ email: email })
+    else{ User.findOne({ email: email })
             .then(user => {
                 if(user) {
                     // User exists
@@ -141,6 +146,7 @@ app.post("/login", async (req, res) => {
                         req.session.isAuth = true;   
                         req.session.user = user;                                   
                         res.redirect('/secret');
+                      
                     }
                     })
 
@@ -152,7 +158,9 @@ app.post("/login", async (req, res) => {
             })
             .catch(err=>{
               console.log(err);
-            })}
+            })
+        }
+}
    
 );
 
@@ -168,12 +176,14 @@ app.get('/detalis',(req,res,next)=>{
 
 app.use('/details', passwordRoutes);
 
-app.get('/:id',(req,res)=>{   
+app.get('/profile',(req,res)=>{   
    //const id = req.params.id;
 
    User.findById(req.session.user._id)
     .then((result) =>{
-     
+      if(!result){
+          console.log('new member');
+      }
        res.render('userprofile',{title:'profile page',profiledata:req.session.user});
     })
     .catch((err) =>{
@@ -185,7 +195,7 @@ app.get('/:id',(req,res)=>{
   );
 
 //profile data update
-app.post('/:id',(req,res,next)=>{     
+app.post('/profile',(req,res,next)=>{     
    let updatedProfile = {
       name:req.body.name,
       email:req.body.email,
@@ -195,8 +205,8 @@ app.post('/:id',(req,res,next)=>{
    User.findByIdAndUpdate(req.session.user._id, {$set: updatedProfile},{ new: true })
     .then((result) =>{
        req.session.user = result;
-       console.log(req.session.user);
-       res.redirect('/'+req.session.user._id);   
+       
+       res.redirect('/profile');   
        
     })
     .catch((err) =>{
@@ -219,7 +229,7 @@ app.post('/:id',(req,res,next)=>{
 */
 
 //logout session
-app.get('/logout',(req,res) => {
+app.post('/logout',(req,res) => {
     req.session.destroy((err) => {
         if(err) {
             return console.log(err);
@@ -230,10 +240,105 @@ app.get('/logout',(req,res) => {
 });
 
 
+
+
 app.get('/',(req,res)=>{  
     res.render('index', { title:"Home Page" });   
 });
 
+app.get('/forgotPassword',(req,res)=>{  
+    res.render('forgotPassword', { title:"Forgot Password Page" });   
+});
+
+app.post('/forgotPassword',async(req,res,next)=>{  
+    //1.get user based on posted email
+    const user = await User.findOne({email:req.body.email});
+
+    if(!user){
+        console.log('There is no emali');
+    }
+ 
+    //2.generate the random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({validateBeforeSave:false});
+
+    //3. send it to user's email
+    const resetURL = `${req.protocol}://${req.get('host')}/resetPassword/${resetToken}`;
+
+    const message  = `forgot your password? Submit a patch request with your new password and repassword to :\n${resetURL} \nIf you did not forget tour password,please ignore this email`;
+  
+    try{
+        await sendEmail({
+        email:user.email,
+        subject:'Your password reset token (valid for 10 min)',
+        message,
+        
+    });
+       console.log('email sent!');
+       res.redirect('/forgotPassword')
+
+    }catch(err){
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({validateBeforeSave:false});
+      //  console.log(err);
+    }
+    //res.render('index', { title:"Home Page" });   
+});
+
+app.get('/resetPassword/:token',(req,res,next)=>{  
+    const token = req.params.token;
+  
+    // User.findOne({token:token})
+    //    .then(() =>{  
+             res.render('resetPassword', { title:"Reset Password Page",tokendata:token});        
+    //    })
+    //    .catch(err=>{
+    //        console.log(err);
+    //    })
+    
+     
+});
+
+
+app.post('/resetPassword/:token',async (req,res,next)=>{  
+
+    
+    //1.get user based on the token
+    const hashedToken =  crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({passwordResetToken:hashedToken,passwordResetExpires:{$gt:Date.now()}});
+
+    //2.If token has not expired,and there is user,set the new password
+    if(!user){
+        console.log('Token is invalid or has expected');
+    }
+
+ 
+   if(req.body.password===req.body.repassword){
+        
+       user.password = req.body.password;
+       user.repassword = req.body.repassword;
+
+       await user.save();
+   
+   }
+   
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+
+    //.3Update changePasswordAt property for the user
+    //4.log te user in,send JWT
+   const token = user._id;
+   
+    // res.status(200).json({
+    //     status:'success',
+    //     token
+    // });
+    console.log('success');
+    res.redirect('/login');
+    
+});
 
 //404
 app.use((req,res)=>{
